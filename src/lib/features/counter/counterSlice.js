c276ebaf5f10b692f03857a55b86c29c79ada522
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-07-11 08:52:59 +0500

    fil

--- src/lib/features/counter/counterSlice.js
+++ src/lib/features/counter/counterSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    value: +localStorage.getItem("counter") || 0,
+    value: localStorage.getItem("counter") || 0,
 }
 
 export const counterSlice = createSlice({
